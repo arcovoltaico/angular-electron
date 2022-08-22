2a65d02293121536da55f924aafb0c2b334927fe
Jordi Alhambra <reflexaction@icloud.com>
2022-08-22 22:24:53 +0100

    Trigger video download and dB analysis from home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {MediaService} from "../services/media.service";
+import {ProvisionService} from "../services/provision.service";
+import {WriteStream} from "fs";
 
 @Component({
   selector: 'app-home',
@@ -7,11 +10,32 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  mainVolume;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private mediaService: MediaService, private provisionService: ProvisionService) {
+  }
 
   ngOnInit(): void {
     console.log('HomeComponent INIT');
+    this.getYoutubeDbs('tvQsyxLR7tk')
+  }
+
+  getYoutubeDbs(YouTubeId: string) {
+
+    this.provisionService.getYouTubeVideo(YouTubeId)
+      .subscribe(
+        (stream) => {
+          console.log('VIDEO DATA is here');
+          this.analiseDbs(stream);
+        });
+  }
+
+  analiseDbs(stream: WriteStream) {
+    this.mediaService.getAudioVolumes(stream)
+      .subscribe(
+        (data) => {
+          this.mainVolume = data['mean_volume'];
+        });
   }
 
 }
