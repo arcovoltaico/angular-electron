f2d120cdba5c084cee9bbb649c3e3dbad3abbbfa
Jordi Alhambra <reflexaction@icloud.com>
2022-09-10 15:54:12 +0100

    fix corss issue

--- app/main.ts
+++ app/main.ts
@@ -23,6 +23,23 @@ function createWindow(): BrowserWindow {
     },
   });
 
+  win.webContents.session.webRequest.onBeforeSendHeaders(
+    (details, callback) => {
+      callback({ requestHeaders: { Origin: '*', ...details.requestHeaders } });
+    },
+  );
+
+  win.webContents.session.webRequest.onHeadersReceived((details, callback) => {
+    callback({
+      responseHeaders: {
+        'Access-Control-Allow-Origin': ['*'],
+        ...details.responseHeaders,
+      },
+    });
+  });
+
+
+
   if (serve) {
     const debug = require('electron-debug');
     debug();
